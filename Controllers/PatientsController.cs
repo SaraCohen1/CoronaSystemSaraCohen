971c6ab9f02b1a71c599a287939b0051b0ed3026
Sara <Sara@DESKTOP-4ESO3PI>
2022-10-23 15:01:19 +0300

    Add project files.

---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CoronaSystem.Data;
+using CoronaSystem.Models;
+
+namespace CoronaSystem.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PatientsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Patients
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Person.ToListAsync());
+        }
+        [HttpPost]
+        public JsonResult AutoComplete(string Prefix)
+        {
+            //Note : you can bind same list from database  
+
+            //Searching records from list using LINQ query  
+            var Context = (from N in _context.Person
+                           where N.MyId.Contains(Prefix)
+                           select new
+                           {
+                               label = N.MyId,
+                               val = N.FirstName
+                           }).ToList();
+            return Json(Context);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Id)
+        {
+            try
+            {
+                Person person = _context.Person
+   .Where(e => e.MyId == Id).FirstOrDefault();
+                return RedirectToAction("Details", "Patients", new { id = person.PersonId });
+            }
+            catch(Exception ex)
+            {
+                throw (ex);
+            }
+           
+        }
+
+        // GET: Patients/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Person
+                .FirstOrDefaultAsync(m => m.PersonId == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: Patients/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PersonId,MyId,FirstName,LastName,Adress,PhoneNumber,CellPhoneNumber,BirthDate,FirstShot,SecondShot,ThirdShot,FourthShot,Vaccine1,Vaccine2,Vaccine3,Vaccine4,PositiveDate,NegativeDate")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: Patients/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Person.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // POST: Patients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PersonId,MyId,FirstName,LastName,Adress,PhoneNumber,CellPhoneNumber,BirthDate,FirstShot,SecondShot,ThirdShot,FourthShot,Vaccine1,Vaccine2,Vaccine3,Vaccine4,PositiveDate,NegativeDate")] Person person)
+        {
+            if (id != person.PersonId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.PersonId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: Patients/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Person
+                .FirstOrDefaultAsync(m => m.PersonId == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Person == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Person'  is null.");
+            }
+            var person = await _context.Person.FindAsync(id);
+            if (person != null)
+            {
+                _context.Person.Remove(person);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(int id)
+        {
+          return _context.Person.Any(e => e.PersonId == id);
+        }
+    }
+}
